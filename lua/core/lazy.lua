b8c9a3b6f9f38cd9bef2a365a6435cfb79618c2e
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2023-11-20 19:49:22 +0800

    feat: add lazy.nvim comment & ignore lazy-lock.json

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -11,4 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Any lua file in ~/.config/nvim/lua/plugins/*.lua will be automatically merged
+-- See https://github.com/folke/lazy.nvim#-structuring-your-plugins
 require("lazy").setup("plugins")
